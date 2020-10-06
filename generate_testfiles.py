22671e83fd56af06bba5f36087b5d787e74a2976
karl <karl.bittner@hexaquo.at>
2020-10-06 17:04:42 +0200

    Generate even number of test numbers
    Also remove the trailing \n

--- generate_testfiles.py
+++ generate_testfiles.py
@@ -1,12 +1,12 @@
 import random
 
-num_iterations = 1000000
+num_iterations = 999999
 
 with open("testdata", "w") as file:
     # Write the number of lines to expect
-    file.write("%s\n" % num_iterations)
+    file.write("%s" % num_iterations)
     
     # Write lots of random numbers
     for i in range(0, num_iterations):
         # Between 0 and the maximum of uint32
-        file.write("%s\n" % random.randint(0, 4294967295))
+        file.write("\n%s" % random.randint(0, 4294967295))
