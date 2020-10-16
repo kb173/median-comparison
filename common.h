60b0bd4f02c0c0446336d67ab85b6d4843c15d06
incredibleLeitman <michael.leithner@gmail.com>
2020-10-17 00:27:51 +0200

    moved compare to common; changed size_t to uint32

--- common.h
+++ common.h
@@ -1,9 +1,23 @@
 #pragma once
 
 // custom swap function
-void swap(size_t* a, size_t* b)
+void swap(uint32_t* a, uint32_t* b)
 {
-    size_t temp = *a;
+    uint32_t temp = *a;
     *a = *b;
     *b = temp;
+}
+
+// comparator function used by qsort
+int compare(const void* a, const void* b)
+{
+    // TODO: check why this does not return the same results???
+    //return (*(uint32_t*)a - *(uint32_t*)b);
+    //return (int)(*(int*)a - *(int*)b);
+    //return (int)(*(const int*)a - *(const int*)b);
+    uint32_t arg1 = *static_cast<const uint32_t*>(a);
+    uint32_t arg2 = *static_cast<const uint32_t*>(b);
+    if (arg1 < arg2) return -1;
+    if (arg1 > arg2) return 1;
+    return 0;
 }
